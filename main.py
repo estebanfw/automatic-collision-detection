5e7a264f84ffe2a9d0071dd146614f892b4fceeb
Esteban <estebanfwenger@gmail.com>
2024-05-01 14:43:35 -0300

    update

---
+++ main.py
@@ -0,0 +1,16 @@
+"""Import packages"""
+import create_dataframe
+import linear_regression
+
+
+def main():
+    """Run full algorithm
+    """
+    df=create_dataframe.main()
+    lr=linear_regression.training(df)
+    test=lr[1]
+    model=lr[2]
+    return linear_regression.testing(model,test)
+
+if __name__ == "__main__": 
+    main()
