9b93136882424729871a9fd82b47383d89760787
Esteban <estebanfwenger@gmail.com>
2024-04-30 23:04:31 -0300

    modules creation

---
+++ cdm/__init__.py
@@ -0,0 +1,36 @@
+"""cdm package"""
+
+""" Pandas Package"""
+import pandas as pd
+from pandas import DataFrame
+
+class Event():
+    """Event Class
+    Conjunction Event Class
+    """
+    def __init__(self, event_id):
+        self.event_id = event_id
+
+class Cdm(Event):
+    """ cdm class
+    """
+    def __init__(self, event_id,time_to_tca, miss_distance,pc, relative_speed,
+       relative_position_r, relative_position_t, relative_position_n,
+       relative_velocity_r, relative_velocity_t, relative_velocity_n,
+       mahalanobis_distance):
+        super().__init__(event_id)
+
+        self.time_to_tca = time_to_tca
+        self.miss_distance = miss_distance
+        self.pc = pc
+        self.relative_position_r = relative_position_r
+        self.relative_position_t = relative_position_t
+        self.relative_position_n = relative_position_n
+        self.relative_velocity_r = relative_velocity_r
+        self.relative_velocity_t = relative_velocity_t
+        self.relative_velocity_n = relative_velocity_n
+        self.relative_speed = relative_speed
+        self.mahalanobis_distance = mahalanobis_distance
+
+    
+
---
+++ data_handling/__init__.py
@@ -0,0 +1,44 @@
+""" data handling package"""
+
+""" Pandas module """
+import pandas as pd
+from pandas import DataFrame
+
+
+def load_data(path:str, fields:list) -> DataFrame:
+    """Function to load data from ESA Challenge Dataset
+
+    Args:
+        path (str): location of the file
+        fields (list): list of fields of interest
+
+    Returns:
+        _type_: Dataframe
+    """
+    df=pd.read_csv(path)
+
+    remove_fields=[]
+    for column in list(df.columns):
+        if column not in fields:
+            remove_fields.append(column)
+    
+    df=df.drop(columns=remove_fields, axis=1)
+
+    return df
+
+def convert_pc_from_log_to_dec(df: DataFrame) -> DataFrame:
+    """Function to convert log 10 risks to decimals
+
+    Args:
+        df (DataFrame): input dataframe with risk in log10
+
+    Returns:
+        DataFrame: output dataframe with risk in decimals
+    """
+    df['pc']=df['risk'].apply(lambda x: 10**x)
+    df.drop("risk", axis='columns',inplace=True)
+    return df
+
+
+
+
