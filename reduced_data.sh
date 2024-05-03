3a5fcafeb753678815011b99ca5d7c2c6b6bfebd
Esteban <estebanfwenger@gmail.com>
2024-05-03 20:14:22 -0300

    fixed path to file

--- reduced_data.sh
+++ reduced_data.sh
@@ -7,7 +7,7 @@ echo "Moving data to data/"
 mv train_data.csv data/
 echo "For testing purposes only a part of the dataframe will be used"
 ls -lh data/
-head --lines 20000 > data/aux.csv
+head --lines 20000 data/train_data.csv > data/aux.csv
 mv data/aux.csv data/train_data.csv
 wc -l data/train_data.csv
 ls -lh 
