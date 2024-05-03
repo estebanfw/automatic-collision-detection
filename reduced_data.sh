e0f091fb9c9ff36329d1f56444cad0d2d27eb6f8
Esteban <estebanfwenger@gmail.com>
2024-05-03 20:13:55 -0300

    use head instead of cat

--- reduced_data.sh
+++ reduced_data.sh
@@ -6,6 +6,7 @@ unzip train_data.zip
 echo "Moving data to data/"
 mv train_data.csv data/
 echo "For testing purposes only a part of the dataframe will be used"
+ls -lh data/
 head --lines 20000 > data/aux.csv
 mv data/aux.csv data/train_data.csv
 wc -l data/train_data.csv
